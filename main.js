68123433a2a2b7b54985ed6a64f53773f551b523
Hafiz Hamza Ahmed Zakai <151182267+hamzahere82@users.noreply.github.com>
2024-03-03 20:12:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// Data types:
+//string:
+var personname = "Hamza";
+console.log(personname);
+//Number:
+var phone_Number = +9245689546;
+console.log(phone_Number);
+// Boolean:
+var question = true;
+console.log(question);
+// Any:
+var Emai = "hamzasohab125@gmail.com";
+console.log(Emai);
