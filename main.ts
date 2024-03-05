bf915739b552c3b593732532d200cfa484a5ed1d
Hafiz Hamza Ahmed Zakai <151182267+hamzahere82@users.noreply.github.com>
2024-03-06 01:23:38 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
-// Data types:
-//string:
-var personname = "Hamza";
-console.log(personname);
-//Number:
-var phone_Number = +9245689546;
-console.log(phone_Number);
-// Boolean:
-var question = true;
-console.log(question);
-// Any:
-var Emai = "hamzasohab125@gmail.com";
-console.log(Emai);
+// let hello = 'Hamza'
+// console.log(`Hello ${hello}, would you like to learn some Python today`);
+// var personName = 'abubakar';
+// console.log(personName.toLowerCase())
+// console.log(personName.toUpperCase())
+// console.log(personName.charAt(0).toUpperCase() + personName.slice(1));
+
+// console.log('Tony Robin oce said, "Setting Goals is the first step in turning the invisible into the vidible" ')
+
+// let famous_perso = 'Tony Robin'
+// let quote ="Setting Goals is the first step in turning the invisible into the vidible"
+// console.log(`${famous_perso} once said, ${quote}`);
+
+// let myName="Hamza";
+// console.log(myName);
+// console.log(myName.trim());
+
+// console.log(4+4);
+// console.log(16-8);
+// console.log(4*2);
+// console.log(32 / 4);
+
+let favourite_number=8;
+console.log(`My favourite number is ${favourite_number}`);
+
+
